1ced2d1ec5af6e3ae28b6dfad0799858a24c0344
Berk Muzaffer Eldemir <beldemir@k1m38s06.42istanbul.com.tr>
2024-10-14 18:21:12 +0300

    aaa

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: beldemir <beldemir@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 15:20:31 by beldemir          #+#    #+#             */
-/*   Updated: 2024/10/13 15:35:58 by beldemir         ###   ########.fr       */
+/*   Updated: 2024/10/14 17:34:41 by beldemir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,16 @@ void	*ft_memset(void *b, int c, size_t len)
 		len--;
 	}
 	return (b);
+}
+
+
+
+// 9876
+#include <stdio.h>
+int main()
+{
+	int a[] = {1,2,3};
+	ft_memset(a, 38, 2); 
+	ft_memset(a, 148, 1);
+	printf("%i\n", a[0]);
 }
